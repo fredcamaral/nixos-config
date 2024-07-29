39a2bf47742483a097f514ad7f88098a9e4bc8b2
Fred Amaral <fred@fredamaral.com.br>
2024-07-29 06:35:08 -0300

    new update

--- flake.nix
+++ flake.nix
@@ -30,7 +30,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+    hyprland = {
+      url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
