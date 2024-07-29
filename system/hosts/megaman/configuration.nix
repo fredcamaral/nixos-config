300e39f3a2a5f4d72913c099e05c631e592bef16
Fred Amaral <fred@fredamaral.com.br>
2024-07-29 11:14:50 -0300

    new update

--- system/hosts/megaman/configuration.nix
+++ system/hosts/megaman/configuration.nix
@@ -10,14 +10,14 @@
   # Define the config for the module networking
   networking = {
     vpnConfig = {
-      enable = false;
+      enable = true;
       hostName = "megaman";
       defaultGatewayAddress = "21.26.7.1";
       nameservers = ["8.8.8.8" "1.1.1.1"];
       networkPrefix = "21.26.7";
       freeFlowInterface = "enp11s0";
       vpnedInterface = "enp10s0";
-      # vpnedPorts = "80,443,8080";
+      vpnedPorts = "80,443,8080";
     };
   };
 
