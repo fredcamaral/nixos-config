801f332d950d6c187b21ed60c0efd0a0abf3c297
Fred Amaral <fred@fredamaral.com.br>
2024-07-29 11:17:55 -0300

    new update

--- system/hosts/sonic/configuration.nix
+++ system/hosts/sonic/configuration.nix
@@ -7,6 +7,20 @@
   ];
 
   ## ! CONFIG HERE: options for proprietary modules ##########################
+  # Define the config for the module networking
+  networking = {
+    hostName = "sonic";
+    # vpnConfig = {
+    #   enable = true;
+    #   defaultGatewayAddress = "21.26.7.1";
+    #   nameservers = ["8.8.8.8" "1.1.1.1"];
+    #   networkPrefix = "21.26.7";
+    #   freeFlowInterface = "enp11s0";
+    #   vpnedInterface = "enp10s0";
+    #   vpnedPorts = "80,443,8080";
+    # };
+  };
+
   # Graphical environment configuration
   graphical = {
     xdefaults.enable = true; # Enable X-Server defaults (true by default)
