c185703a80bd60fdb8d31518f6eb9772b946f6a3
Fred Amaral <fred@fredamaral.com.br>
2024-07-29 19:42:28 -0300

    new update

--- system/hosts/sonic/modules/services.nix
+++ system/hosts/sonic/modules/services.nix
@@ -6,10 +6,10 @@
 }: {
   services = {
     # Enable OpenRGB for RGB lighting control
-    hardware.openrgb.enable = false;
+    hardware.openrgb.enable = true;
 
     # Add OpenRGB udev rules
-    # udev.packages = with pkgs; [openrgb];
+    udev.packages = with pkgs; [openrgb];
 
     # Enable periodic TRIM for SSDs
     fstrim.enable = true;
@@ -38,16 +38,6 @@
       alsa.enable = true;
       alsa.support32Bit = true;
       pulse.enable = true;
-    };
-
-    # Enable OllaMa for ROCm
-    ollama = {
-      enable = true;
-      acceleration = "rocm";
-      environmentVariables = {
-        HCC_AMDGPU_TARGET = "gfx1102"; # used to be necessary, but doesn't seem to anymore
-      };
-      rocmOverrideGfx = "11.0.0";
     };
   };
 }
