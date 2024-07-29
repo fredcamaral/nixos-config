6f7fe40f9553f88b1fc39c92ec9714be9adf54a5
Fred Amaral <fred@fredamaral.com.br>
2024-07-29 20:45:12 -0300

    new update

--- system/common/secrets/default.nix
+++ system/common/secrets/default.nix
@@ -4,10 +4,18 @@
   ...
 }: {
   age = {
-    identityPaths = ["${config.users.users.fredamaral.home}/repos/.secrets/${hostname}-tailscale.txt"];
+    identityPaths = ["${config.users.users.fredamaral.home}/repos/.secrets/${hostname}.txt"];
 
     secrets."${hostname}-tailscale-auth" = {
       file = "${config.users.users.fredamaral.home}/repos/.secrets/${hostname}-tailscale-auth.age";
+
+      owner = "root";
+      group = "root";
+      mode = "0400";
+    };
+
+    secrets."${hostname}-wifi-farenet" = {
+      file = "${config.users.users.fredamaral.home}/repos/.secrets/${hostname}-wifi-farenet.age";
 
       owner = "root";
       group = "root";
--- system/common/secrets/secrets.nix
+++ system/common/secrets/secrets.nix
@@ -1,10 +1,23 @@
 {
+  # Tailscale public keys for the Megaman and Sonic machines.
   "megaman-tailscale-auth.age" = {
     publicKeys = [
       "age1xf7ukm9upngpd3547s6jwqvw0hnekmm6mpm5gdrw3t2chwu9z4vs4jfmvq"
     ];
   };
   "sonic-tailscale-auth.age" = {
+    publicKeys = [
+      "age1w3hm6rc5lft2eknd547ytcdl2aptcn8pez8srduwvezt2nyk635s374wde"
+    ];
+  };
+
+  # Farenet public keys for the Megaman and Sonic machines.
+  "megaman-wifi-farenet.age" = {
+    publicKeys = [
+      "age1xf7ukm9upngpd3547s6jwqvw0hnekmm6mpm5gdrw3t2chwu9z4vs4jfmvq"
+    ];
+  };
+  "sonic-wifi-farenet.age" = {
     publicKeys = [
       "age1w3hm6rc5lft2eknd547ytcdl2aptcn8pez8srduwvezt2nyk635s374wde"
     ];
