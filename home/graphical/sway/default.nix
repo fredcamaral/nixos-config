1455f5005b838d4f8d0c43122ce7fa2e8cdea1e9
Fred Amaral <fred@fredamaral.com.br>
2024-07-30 15:05:29 -0300

    new update

--- home/graphical/sway/default.nix
+++ home/graphical/sway/default.nix
@@ -171,8 +171,8 @@ in {
         "${modifier}+Shift+v" = "floating toggle";
 
         # Copy and paste
-        "Ctrl+c" = "exec ${pkgs.wl-clipboard}/bin/wl-copy";
-        "Ctrl+v" = "exec ${pkgs.wl-clipboard}/bin/wl-paste";
+        # "Ctrl+c" = "exec ${pkgs.wl-clipboard}/bin/wl-copy";
+        # "Ctrl+v" = "exec ${pkgs.wl-clipboard}/bin/wl-paste";
 
         # Switch to the specified workspace number.
         "${modifier}+1" = "workspace number 1";
