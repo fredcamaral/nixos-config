1e0b1cb335d0a5046b7a8f5f0e7817a5f1c09aa9
Fred Amaral <fred@fredamaral.com.br>
2024-07-30 18:27:53 -0300

    new update

--- system/common/console.nix
+++ system/common/console.nix
@@ -1,7 +1,6 @@
 # system/modules/console.nix
 {pkgs, ...}: {
   console = {
-    # Set the console font
     # This uses the Terminus font, which is good for high-resolution displays
     font = "${pkgs.terminus_font}/share/consolefonts/ter-v32n.psf.gz";
 
