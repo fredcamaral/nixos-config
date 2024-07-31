61f2dc16b1aedbaec21d0a9ee5abfd3413aae33d
Fred Amaral <fred@fredamaral.com.br>
2024-07-31 18:11:16 -0300

    new update

--- home/graphical/sway/waybar/default.nix
+++ home/graphical/sway/waybar/default.nix
@@ -38,6 +38,27 @@
       };
     };
 
+    cava = {
+      # cava_config = "$XDG_CONFIG_HOME/cava/cava.conf";
+      framerate = 60;
+      autosens = 0;
+      sensitivity = 2;
+      bars = 14;
+      lower_cutoff_freq = 50;
+      higher_cutoff_freq = 10000;
+      method = "pulse";
+      source = "auto";
+      stereo = true;
+      reverse = false;
+      bar_delimiter = 0;
+      monstercat = true;
+      waves = false;
+      noise_reduction = 0.77;
+      reverse_order = true;
+      input_delay = 1;
+      format-icons = ["▁" "▂" "▃" "▄" "▅" "▆" "▇" "█"];
+    };
+
     "sway/window" = {
       format = "󰖯 {}";
       max-length = 100;
@@ -222,6 +243,7 @@ in {
           "sway/mode"
           "network#nic1"
           "network#nic2"
+          "cava"
         ];
         modules-center = ["sway/window"];
         modules-right = [
@@ -263,6 +285,7 @@ in {
           "custom/user-host-label"
           "clock"
           "idle_inhibitor"
+          "cava"
           ;
       }
       {
