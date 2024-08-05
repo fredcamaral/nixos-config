5e3ce118d77312d848dabc4d557264dc526da851
Fred Amaral <fred@fredamaral.com.br>
2024-08-05 20:57:01 -0300

    new update

--- system/hosts/bomberman/modules/packages.nix
+++ system/hosts/bomberman/modules/packages.nix
@@ -7,6 +7,30 @@
     niv
     rxvt_unicode
     xclip
+    firefox
+    kitty
+    vscode
+    zellij
+    git
+    atuin
+    bat
+    bottom
+    btop
+    direnv
+    eza
+    fzf
+    htop
+    jq
+    lazygit
+    micro
+    neovim
+    nix-index
+    ripgrep
+    tmux
+    yazi
+    zathura
+    zoxide
+    zsh
 
     # For hypervisors that support auto-resizing, this script forces it.
     # I've noticed not everyone listens to the udev events so this is a hack.
