c566c72f1f9557b35b619b47f5a294a1d7ba5fbe
Fred Amaral <fred@fredamaral.com.br>
2024-08-05 14:18:48 -0300

    new update

--- home/programs.nix
+++ home/programs.nix
@@ -76,7 +76,7 @@
         # List of OBS plugins
         obs-studio-plugins.obs-gstreamer
         obs-studio-plugins.obs-vkcapture
-        obs-studio-plugins.obs-pipewire-audio-capture
+        # obs-studio-plugins.obs-pipewire-audio-capture
         obs-studio-plugins.obs-multi-rtmp
         obs-studio-plugins.obs-move-transition
       ];
