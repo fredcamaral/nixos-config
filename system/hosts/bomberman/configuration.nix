24869d1c9f388c7143781b71dd27096d7a24d091
Fred Amaral <fred@fredamaral.com.br>
2024-08-05 16:57:46 -0300

    new update

--- system/hosts/bomberman/configuration.nix
+++ system/hosts/bomberman/configuration.nix
@@ -31,8 +31,6 @@
   #   };
   # };
 
-  ## ! CONFIG HERE: options for proprietary modules ##########################
-  # Define the config for the module networking
   networking = {
     hostName = hostname;
     networkmanager.enable = true;
@@ -40,18 +38,15 @@
 
   # Graphical environment configuration
   graphical = {
-    xdefaults.enable = true; # Enable X-Server defaults (true by default)
+    xdefaults.enable = false; # Enable X-Server defaults (true by default)
     managers = {
-      enable = true; # Enable graphical environment (false by default)
-
-      # Choose one of the following (or none for default GNOME):
+      enable = false; # Enable graphical environment (false by default)
       # gnome.enable = true;
       # hyprland.enable = true;
-      sway.enable = true;
+      # sway.enable = true;
       # i3.enable = true;
     };
   };
-  ## ! END CONFIG #############################################################
 
   # Specify the NixOS release version
   # This value determines the NixOS release from which the default
