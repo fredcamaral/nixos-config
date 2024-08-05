6fa68c50a7883c6e72114c91e9c6147c214c05c4
Fred Amaral <fred@fredamaral.com.br>
2024-08-05 18:39:08 -0300

    new update

--- system/hosts/bomberman/modules/console.nix
+++ system/hosts/bomberman/modules/console.nix
@@ -2,7 +2,7 @@
 {pkgs, ...}: {
   console = {
     # This uses the Terminus font, which is good for high-resolution displays
-    font = "${pkgs.nerdfonts}/share/fonts/truetype/NerdFonts/DejaVuSansMono Nerd Font Complete Mono-18.ttf";
+    font = "${pkgs.nerdfonts}/share/fonts/truetype/NerdFonts/DejaVuSansMono Nerd Font Complete Mono.ttf";
 
     # Use the X keyboard configuration for the console as well
     useXkbConfig = true;
