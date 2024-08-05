a187cb27e9ab4c2075f83b642f5495453f2f7943
Fred Amaral <fred@fredamaral.com.br>
2024-08-05 18:32:40 -0300

    new update

--- system/common/secrets/default.nix
+++ system/common/secrets/default.nix
@@ -13,21 +13,5 @@
       group = "root";
       mode = "0400";
     };
-
-    secrets."${hostname}-wifi-farenet" = {
-      file = "${config.users.users.fredamaral.home}/repos/.secrets/${hostname}-wifi-farenet.age";
-
-      owner = "root";
-      group = "root";
-      mode = "0400";
-    };
-
-    secrets."${hostname}-wireless-networks" = {
-      file = "${config.users.users.fredamaral.home}/repos/.secrets/${hostname}-wireless-networks.age";
-
-      owner = "root";
-      group = "root";
-      mode = "0400";
-    };
   };
 }
--- system/common/secrets/secrets.nix
+++ system/common/secrets/secrets.nix
@@ -4,29 +4,7 @@
       "age1xf7ukm9upngpd3547s6jwqvw0hnekmm6mpm5gdrw3t2chwu9z4vs4jfmvq"
     ];
   };
-  "sonic-tailscale-auth.age" = {
-    publicKeys = [
-      "age1w3hm6rc5lft2eknd547ytcdl2aptcn8pez8srduwvezt2nyk635s374wde"
-    ];
-  };
-
-  "megaman-wifi-farenet.age" = {
-    publicKeys = [
-      "age1xf7ukm9upngpd3547s6jwqvw0hnekmm6mpm5gdrw3t2chwu9z4vs4jfmvq"
-    ];
-  };
-  "sonic-wifi-farenet.age" = {
-    publicKeys = [
-      "age1w3hm6rc5lft2eknd547ytcdl2aptcn8pez8srduwvezt2nyk635s374wde"
-    ];
-  };
-
-  "megaman-wireless-networks.age" = {
-    publicKeys = [
-      "age1xf7ukm9upngpd3547s6jwqvw0hnekmm6mpm5gdrw3t2chwu9z4vs4jfmvq"
-    ];
-  };
-  "sonic-wireless-networks.age" = {
+  "bomberman-tailscale-auth.age" = {
     publicKeys = [
       "age1w3hm6rc5lft2eknd547ytcdl2aptcn8pez8srduwvezt2nyk635s374wde"
     ];
