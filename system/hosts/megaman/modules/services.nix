9dd24ae3ce71705a256948f966611df0f9368001
Fred Amaral <fred@fredamaral.com.br>
2024-08-11 20:44:51 -0300

    new update

--- system/hosts/megaman/modules/services.nix
+++ system/hosts/megaman/modules/services.nix
@@ -14,17 +14,6 @@
     # Enable CUPS for printing
     printing.enable = true;
 
-    udev = {
-      enable = true;
-      extraRules = ''
-        SUBSYSTEM=="usb", ATTRS{idVendor}=="0fd9", ATTRS{idProduct}=="0060", MODE:="666", GROUP="plugdev", SYMLINK+="streamdeck"
-        SUBSYSTEM=="usb", ATTRS{idVendor}=="0fd9", ATTRS{idProduct}=="006d", MODE:="666", GROUP="plugdev", SYMLINK+="streamdeck"
-        SUBSYSTEM=="usb", ATTRS{idVendor}=="0fd9", ATTRS{idProduct}=="0080", MODE:="666", GROUP="plugdev", SYMLINK+="streamdeck"
-        SUBSYSTEM=="usb", ATTRS{idVendor}=="0fd9", ATTRS{idProduct}=="0063", MODE:="666", GROUP="plugdev", SYMLINK+="streamdeck-mini"
-        SUBSYSTEM=="usb", ATTRS{idVendor}=="0fd9", ATTRS{idProduct}=="006c", MODE:="666", GROUP="plugdev", SYMLINK+="streamdeck-xl"
-      '';
-    };
-
     # Enable Tailscale VPN
     tailscale = {
       enable = true;
