da5a70508c74325d74e7a20aee24bc372f6be91b
Fred Amaral <fred@fredamaral.com.br>
2024-08-30 10:48:17 -0300

    new update

--- system/hosts/megaman/modules/zfs.nix
+++ system/hosts/megaman/modules/zfs.nix
@@ -6,7 +6,8 @@
 }: {
   boot = {
     # Use the latest Linux kernel compatible with ZFS
-    kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+    # kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+    kernelPackages = pkgs.linuxPackages;
 
     # Enable ZFS support
     supportedFilesystems = ["zfs" "exfat"];
